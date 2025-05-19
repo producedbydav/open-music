12840cbcd79651bf0c577bb6415a935ef3760fb5
dav <94023975+producedbydav@users.noreply.github.com>
2025-05-19 15:12:25 -0700

    Create BtB_renderer.sol

---
+++ Beginning to Believe/BtB_renderer.sol
@@ -0,0 +1,288 @@
+/*▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░▒▒▓▓▓▓░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▒▒▒░░░░░░░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░▒▒▓▓▓▓░░▓▓░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▓▓▓▓▓▓▒▒▒░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒░░░▒▒▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▓▓▓▓▓▓▒▒▒░░░░░░░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▒▒▒░░░░░░░░░▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▒▒░░░░░▒▒▒▒▒░░░░░░░░░▒▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░▒▓▓▓▓▓▒▒▒░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▒░░░░▒▒▒▓▓▓▓▓▓▒▒░░░░░░▒▓▓▓▓▓░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▒▒░░░▒▒▓▓▓▓▓▓▓▓▓▓▒░░░░░▒▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▒░░░░▒▓▓▓▓▓▓▓▓▓▓▓▒░░░░▒▒▓▓▓▓▓▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▓▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▒░░░░▒▓▓▓▓▓▓▓▓▓▓▓▒░░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░▒▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▒▒░░░▒▒▓▓▓▓▓▓▓▓▓▒▒░░░░░▒▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▒▒░░░░▒▒▓▓▓▓▓▒▒░░░░░░░▒▓▓▓▓▒░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░▒▒▒▒▒▒▒▒▒▓▓▓▓▓▒▒▒
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▒▒░░░░░░░▒░░░░░░░░░░░░▒▒▓▓▒░▒▒░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░▒▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░░░░░░░░░░░░░▒▒▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒░░░░░░░▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░░░░░░░░░▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓Beginning to Believe▓▓▓▓▓▓▓▓▓▓▓▒▒▒░░░░░░░░░░░░░░░▒▒▒▒(CC0)▒▒▒░░
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓░░▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./base64.sol";
+import "./IFileStore.sol";
+import "./IT0nev1.sol";
+import "./ILens.sol";
+
+interface IERC721 {
+    function ownerOf(uint tokenId) external view returns (address);
+    function refreshMetadata(uint _tokenId) external; 
+}
+
+interface ILensAlgorithm {
+    function currentLens() external view returns (uint);
+}
+
+interface ISVG {
+    function generateSVG(string memory _text, bytes[2] memory _colors, uint[6][] memory _values) external view returns (string memory);
+}
+
+interface IJavaScript {
+    function generateScript() external view returns (string memory);
+}
+
+contract beginning {
+
+    address public Owner;
+
+    IERC721 public tokenContract;
+    ISVG public svgExtention;
+    IJavaScript public jsExtension;
+    IFileStore public fileStore;
+    ILensAlgorithm public lensAlgo;
+
+    mapping (uint => ILens) public lenses;
+    mapping (uint => address) public satellites;
+
+    uint[] public lensIds;
+    uint[] public satelliteIds;
+
+    uint public currentLensVar = 1;
+    bool public lensControlAlgorithmic;
+    bool public lensControlDelegated;
+    address public lensDelegate;
+
+    string private notAuth = "Not authorized";
+
+    constructor ()
+    {
+        Owner = msg.sender;
+        tokenContract = IERC721(0x5e252195D5887D2456212E7A8B401B0c9FB869d1);
+        svgExtention = ISVG(0x5ec56076f2Ec957079312DeA2b6F96A4Ed73f43d);
+        fileStore = IFileStore(0xFe1411d6864592549AdE050215482e4385dFa0FB);
+        lenses[1] = ILens(0xC00C8a5136bd05B8BCe9FFAce148F86c7ACCc1eb);
+
+        lensIds.push(1);
+    }
+
+    function tokenURI(uint256 _tokenId) external view returns(string memory) {
+        require(_tokenId == 1, "One.");
+        string memory svg = generateSVG();
+        string memory traits = tokenToTraits();
+        string memory htmlCode = generateHTMLwSVG(svg);
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name":"Beginning to Believe', 
+                        '","image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(svg)),
+                        '","animation_url":"data:text/html;base64,', htmlCode, 
+                        '","attributes": [',
+                            traits,
+                        ']}'
+                    )
+                )
+            )
+        );
+        return string(abi.encodePacked('data:application/json;base64,', json));
+    }
+
+    function tokenToTraits() private view returns (string memory) {
+        string memory traits = string(abi.encodePacked(
+            '{"trait_type": "Lens", "value": "', lenses[currentLens()].name(), '"} '
+        ));
+        return traits;
+    }
+
+    function generateSVG() public view returns (string memory) {
+        return svgExtention.generateSVG(lenses[currentLens()].text(), lenses[currentLens()].colors(), lenses[currentLens()].svgValues());
+    }
+
+    function generateScript() public view returns (string memory) {
+        return jsExtension.generateScript();
+    }
+
+    function generateHTMLwSVG(string memory _svg) private view returns (string memory) {
+        return Base64.encode(
+            abi.encodePacked(
+                "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Beginning to Believe</title><style>* {margin: 0; padding: 0; border: 0;} body{overflow:hidden;} #playArea{width:100%;height:100vh;display: flex;justify-content:center;align-items:center;cursor:pointer;position:relative;} .lighted {animation-name:light;animation-duration:0.2s;animation-iteration-count:infinite;} @keyframes light{0%{scale:1;}20%{scale:1.002;}40%{scale:1.004;}50%{scale:1.005;}60%{scale:1.004;}80%{scale:1.002;}100%{scale:1;}}</style></head><body>",
+                _svg,
+                getTonejs(),
+                "<script>",
+                generateScript(),
+                "</script></body></html>"
+            )
+        );
+    }
+    
+    function generateHTML() public view returns (string memory) {
+        string memory svg = generateSVG();
+        return generateHTMLwSVG(svg);
+    }
+
+    function currentLens() public view returns (uint) {
+        return lensControlAlgorithmic ? lensAlgo.currentLens() : currentLensVar;
+    }
+
+    function holderToggleLensCntrlDelegated() public {
+        require (msg.sender == tokenContract.ownerOf(1), notAuth);
+        lensControlDelegated = !lensControlDelegated;
+    }
+
+    function holderToggleLensCntrlAlgorithmic() public {
+        if (!lensControlDelegated) {require (msg.sender == tokenContract.ownerOf(1), notAuth);}
+        if (lensControlDelegated) {require (msg.sender == lensDelegate, notAuth);}
+        lensControlAlgorithmic = !lensControlAlgorithmic;
+    }
+
+    function setLensAlgorithm(address _algo) public {
+        require(msg.sender == Owner, notAuth);
+        lensAlgo = ILensAlgorithm(_algo);
+    }
+
+    function holderSetLensDelegate(address _delegate) public {
+        require (msg.sender == tokenContract.ownerOf(1), notAuth);
+        lensDelegate = _delegate;
+    }
+
+    function holderSetCurrentLens(uint _newLens) public {
+        if (!lensControlDelegated) {require (msg.sender == tokenContract.ownerOf(1), notAuth);}
+        if (lensControlDelegated) {require (msg.sender == lensDelegate, notAuth);}
+        require (indexOf(_newLens) != -1, "Lens doesn't exist");
+        currentLensVar = _newLens;
+        tokenContract.refreshMetadata(1);
+    }
+
+    function updateLenses(uint _id, address _newLens) public {
+        require(msg.sender == Owner, notAuth);
+
+        if (_newLens == address(0)) {
+            if (address(lenses[_id]) != address(0)) {
+                delete lenses[_id];
+                _removeLensId(_id);
+            }
+        } else {
+            if (address(lenses[_id]) == address(0)) {
+                lensIds.push(_id);
+            }
+            lenses[_id] = ILens(_newLens);
+        }
+    }
+
+    function _removeLensId(uint _id) private {
+        uint length = lensIds.length;
+        for (uint i = 0; i < length; i++) {
+            if (lensIds[i] == _id) {
+                lensIds[i] = lensIds[length - 1];
+                lensIds.pop();
+                break;
+            }
+        }
+    }
+
+    function updateSatellites(uint _id, address _newSat) public {
+        require(msg.sender == Owner, notAuth);
+
+        if (_newSat == address(0)) {
+            if (address(satellites[_id]) != address(0)) {
+                delete satellites[_id];
+                _removeSatId(_id);
+            }
+        } else {
+            if (address(satellites[_id]) == address(0)) {
+                satelliteIds.push(_id);
+            }
+            satellites[_id] = _newSat;
+        }
+    }
+
+    function _removeSatId(uint _id) private {
+        uint length = satelliteIds.length;
+        for (uint i = 0; i < length; i++) {
+            if (satelliteIds[i] == _id) {
+                satelliteIds[i] = satelliteIds[length - 1];
+                satelliteIds.pop();
+                break;
+            }
+        }
+    }
+
+    function transferOwnership(address _newOwner) public {
+        require (msg.sender == Owner, notAuth);
+        Owner = _newOwner;
+    }
+
+    function setJSExtension(address _js) public {
+        require (msg.sender == Owner, notAuth);
+        jsExtension = IJavaScript(_js);
+    }
+
+    function setSVGExtension(address _svg) public {
+        require (msg.sender == Owner, notAuth);
+        svgExtention = ISVG(_svg);
+    }
+
+    string toneString = "Tone.min.js.gz";
+
+    function updateToneString(string memory _new) public returns (string memory){
+        require (msg.sender == Owner, notAuth);
+        toneString = _new;
+        return toneString;
+    }
+
+    function getTonejs() public view returns (string memory) {
+        return string.concat(
+            "<script type=\"text/javascript+gzip\" src=\"data:text/javascript;base64,",
+            fileStore.getFile(toneString).read(),
+            "\"></script>",
+            getUNZIP()
+        );
+    }
+
+    function getUNZIP() internal view returns (string memory) {
+        return string.concat(
+            "<script src=\"data:text/javascript;base64,",
+            fileStore.getFile("gunzipScripts-0.0.1.js").read(),
+            "\"></script>"
+        );
+    }
+
+    function indexOf(uint _value) internal view returns (int) {
+        for (uint i = 0; i < lensIds.length; i++) {
+            if (lensIds[i] == _value) {
+                return int(i);
+            }
+        }
+        return -1;
+    }
+
+    function getLensIds() public view returns (uint[] memory) {
+        return lensIds;
+    }
+
+    function getSatelliteIds() public view returns (uint[] memory) {
+        return satelliteIds;
+    }
+
+}
